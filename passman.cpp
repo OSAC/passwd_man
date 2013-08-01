eb10a7d6a9253a0d2e2d79f3c603e94be71ffac1
someone <someone@s.(none)>
2013-08-01 14:58:38 +0545

    minor editing

--- passman.cpp
+++ passman.cpp
@@ -5,8 +5,10 @@ int main(int argc, char** argv)
 char site[200],comm[200];
 if(argc <2 )
 	{
-	sprintf(comm,"info %s",argv[0]);
-	system(comm);
+	printf("Usage %s -i              Initialize only for first time use.\n",argv[0]);
+	printf("%s -s \'site\'           Store password for site named \'site\'\n",argv[0]);
+	printf("%s -g \'site\'           Get password for site named \'site\'\n",argv[0]);
+	
 	return 1;
 }
 else if( (argc == 2) && (strcmp(argv[1],"-i") ==0) )
